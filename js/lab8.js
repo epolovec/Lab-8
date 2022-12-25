5ee40d4a3554403dbc07eca2f9463b05e1f350b4
epolovec <92921634+epolovec@users.noreply.github.com>
2022-12-26 01:29:06 +0200

    main part done

--- js/lab8.js
+++ js/lab8.js
@@ -1,29 +1,3 @@
 function showbar() {
     document.getElementById("search-bar").style.display = "block";
 }
-
-const image = document.getElementById('image');
-const background = document.querySelector('.background');
-
-image.addEventListener('load', () => {
-  background.style.height = `${image.offsetHeight}px`;
-});
-
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
-
-// Get the navbar
-var navbar = document.getElementById("navbar");
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-} 
-
