3d3561efeef34f9c8c8aa64112f0643054c9b834
epolovec <92921634+epolovec@users.noreply.github.com>
2022-12-26 14:25:05 +0200

    fixed nav and added to body

--- js/lab8.js
+++ js/lab8.js
@@ -1,3 +1,17 @@
 function showbar() {
     document.getElementById("search-bar").style.display = "block";
 }
+
+  // wait for the DOM to load
+  window.onload = function() {
+    // select all images
+    const images = document.querySelectorAll('.images img');
+
+    // loop through the images
+    for (let i = 0; i < images.length - 1; i++) {
+      // if the image is the last on its row (i.e., it has a right neighbor), remove the margin
+      if (images[i].offsetTop === images[i + 1].offsetTop) {
+        images[i].classList.add('no-margin');
+      }
+    }
+  };
